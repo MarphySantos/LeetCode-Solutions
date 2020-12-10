282ff0434fb8ab42e41ad7d3f2a0730520c8d337
MarphySantos <72994149+MarphySantos@users.noreply.github.com>
2020-12-09 23:30:22 -0800

    Update leetCode888.cpp

--- Optimized/leetCode888.cpp
+++ Optimized/leetCode888.cpp
@@ -4,9 +4,8 @@ public:
         int diff = accumulate(B.begin(), B.end(), 0) - accumulate(A.begin(), A.end(), 0);
         diff /= 2;
         unordered_set<int> set;
-        for(int i = 0; i < B.size(); i++){
+        for(int i = 0; i < B.size(); i++)
             set.insert(B[i]);
-        }
         for(int i = 0; i < A.size(); i++){
             if(set.count(A[i] + diff))
                 return {A[i], A[i] + diff};
