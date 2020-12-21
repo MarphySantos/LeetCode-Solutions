6c6f96f85a1bc2997e8e0327991d1edea0a0f4b1
MarphySantos <72994149+MarphySantos@users.noreply.github.com>
2020-12-20 23:12:49 -0800

    Update leetCode136.cpp

--- Optimized/leetCode136.cpp
+++ Optimized/leetCode136.cpp
@@ -3,9 +3,8 @@ public:
     int singleNumber(vector<int>& nums) {
         int len = nums.size();
         int answer = 0;
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < len; i++)
             answer ^= nums[i];
-        }
         return answer;
     }
 };
