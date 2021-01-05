7717d27c61404f4f4bde9305a46cfe7f3180dd1b
MarphySantos <72994149+MarphySantos@users.noreply.github.com>
2021-01-04 23:55:30 -0800

    Update leetCode1365.cpp

--- Optimized/leetCode1365.cpp
+++ Optimized/leetCode1365.cpp
@@ -5,22 +5,17 @@ public:
         int len = nums.size();
         vector<int> ans;
         int counter[101];
-        for(int i = 0; i < 101; i++){
+        for(int i = 0; i < 101; i++)
             counter[i] = 0;
-        }
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < len; i++)
             counter[nums[i]]++;
-        }
-        for(int i = 1; i < 101; i++){
+        for(int i = 1; i < 101; i++)
             counter[i] += counter[i-1];
-        }
         for(int i = 0; i < len; i++){
-            if(nums[i] == 0){
+            if(nums[i] == 0)
                 ans.push_back(0);
-            }
-            else{
+            else
                 ans.push_back(counter[nums[i] - 1]);
-            }
         }
         return ans;
     }
